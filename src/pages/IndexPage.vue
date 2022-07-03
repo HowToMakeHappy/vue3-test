a280185725b32862d7692fc76b90e07c66b1fa43
HowToMakeHappy <howtomakehappy.com@gmail.com>
2022-07-03 10:01:30 +0900

    init commit.

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="increment">
+    Count is {{count}}, Double is {{double}}
+  </button>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data () {
+    return {
+      count: 0
+    }
+  },
+  computed: {
+    double () {
+      return this.count * 2
+    }
+  },
+  methods: {
+    increment () {
+      this.count++
+    }
+  },
+  name: 'IndexPage'
+})
+</script>
